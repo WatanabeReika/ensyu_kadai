8b5325241eb719e6d6f899f76f502a138d913fd4
ReikaWatanabe <b1017125@fun.ac.jp>
2019-06-17 16:12:32 +0900

    aaa

---
+++ practice/practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by 渡邊峰香 on 2019/06/17.
+//  Copyright © 2019 渡邊峰香. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
